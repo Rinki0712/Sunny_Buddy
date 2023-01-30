4e6e00a7c2fed07672730732581b906e949ba42f
Rinki0712 <79964244+Rinki0712@users.noreply.github.com>
2023-01-30 11:33:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,34 +35,3 @@ submit.addEventListener("click",(e)=>{
 })
 getWeather("Delhi")// search for weather of Delhi.
 
-/* Function declaration for getting details of common cities.
-
-const getWeather2=(city2)=>{ //function to get the weather for different city that is searched on page.
-    cityName.innerHTML=city2
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+ city2, options) //fetching data from weather api for given city name
-    .then(response => response.json())
-    .then(response => {
-
-        console.log(response)
-//different weather conditions.
-        cloud_pct2.innerHTML = response.cloud_pct
-        temp2.innerHTML = response.temp 
-        feels_like2.innerHTML = response.feels_like
-        humidity2.innerHTML = response.humidity
-        min_temp2.innerHTML = response.min_temp
-        max_temp2.innerHTML = response.max_temp
-        wind_speed2.innerHTML = response.wind_speed
-        wind_degrees2.innerHTML = response.wind_degrees
-        sunrise2.innerHTML = response.sunrise
-        sunset2.innerHTML = response.sunset
-
-
-    })
-    .catch(err => console.error(err)); //to handle error
-}
-submit.addEventListener("click",(e)=>{
-    e.preventDefault()//prevents page to reload
-    getWeather2(city2.value)
-})
-getWeather2("Shanghai")
-*/
